dca517ba2a7bb3bd52473a104d447b0ce10d6640
Adrian Bourdelande <adrianbourdelande@gmail.com>
2023-11-08 19:20:15 +0100

    Javascript and CSS updated ==> Added graphic interface and implemented with the logic

--- Blackjack/Blackjack.js
+++ Blackjack/Blackjack.js
@@ -1,4 +1,5 @@
-// let name = prompt("Introduzca su nombre");
+//let name = prompt("Introduzca su nombre");
+let name = "Adrian";
 //clubs (♣), diamonds (♦), hearts (♥) and spades (♠)
 let baraja = [
                 ["A_c","2_c","3_c","4_c","5_c","6_c","7_c","8_c","9_c","10_c","J_c","Q_c","K_c"],
@@ -8,6 +9,25 @@ let baraja = [
 ];
 let mano_J = [];
 let mano_D = [];
+let contador_D = 0;
+let contador_J = 0;
+let i = 0;
+let l = 0;
+let tiempo = 0;
+let nuevaImagen=[];
+let nuevaImagenJugador=[];
+let puntuacion_D=[];
+puntuacion_D[0]=0;
+let puntuacion_J=[];
+puntuacion_J[0]=0;
+const pedirButton = document.getElementById("pedirButton");
+const plantarseButton = document.getElementById("plantarseButton");
+const jugadorNombre = document.getElementById("jugadornombre");
+const dealer = document.getElementById("dealer");
+const puntuacionDealer = document.getElementById("puntuacionDealer");
+const puntuacionJugador = document.getElementById("puntuacionJugador");
+const jugador = document.getElementById("jugador");
+const tablero = document.getElementById("tablero");
 
 function numeroAleatorio(min, max) {
     // The maximum is inclusive and the minimum is inclusive
@@ -49,33 +69,78 @@ function valorarCarta (carta){
     return valor;
 }
 
-//Inicio del juego // El Dealer se saca sus cartas hasta llegar a sumar 17 o más
-mano_D=[    baraja[numeroAleatorio(0,3)][numeroAleatorio(0,12)],
-            baraja[numeroAleatorio(0,3)][numeroAleatorio(0,12)]];
-console.log(`mano inicial del Dealer ${mano_D}`);
-let contador_D = 0;
-let contador_J = 0;
-let i = 0;
-while(contador_D<17){
-    contador_D= contador_D+ valorarCarta(mano_D[i]);
-    i = i+1;    
-    if(i>1&&contador_D<17){        
-        pedirCarta(mano_D);
+function pintarTablero (){
+    console.log("Inicio funcion tablero: la mano del Dealer es: "+mano_D);
+    for(let j=0; j<mano_D.length; j++){             
+        setTimeout(() => {
+            dealer.appendChild(nuevaImagen[j]); 
+            dealer.children[j].setAttribute("src", `./img/${mano_D[j]}.png`); 
+            puntuacionDealer.textContent = `Puntuacion Dealer: ${puntuacion_D[j]}`;
+            setTimeout(() => {
+                if(contador_D>21&&(j==mano_D.length-1)){
+                    let resultado = document.createElement("div");
+                    tablero.appendChild(resultado);
+                    resultado.textContent = `${name} ha ganado`;
+                    resultado.className = "resultado";
+                }                 
+            }, 1000);                    
+        }, tiempo= tiempo+2000);
     }    
 }
-console.log(`El Dealer ha sacado un total de : ${contador_D} puntos`);
-console.log(`Esta es la mano del DEALER ${mano_D}`);
 
-//Turno del jugador
-mano_J=[    baraja[numeroAleatorio(0,3)][numeroAleatorio(0,12)],
-            baraja[numeroAleatorio(0,3)][numeroAleatorio(0,12)]];
-console.log(`mano inicial del JUGADOR ${mano_J}`);
-contador_J= contador_J+ valorarCarta(mano_J[0])+ valorarCarta(mano_J[1]);
-console.log(`mano actual del JUGADOR vale ${contador_J}`);
-i=2;
-while(confirm("Pedir carta?")){
-    pedirCarta(mano_J);
-    contador_J = contador_J + valorarCarta(mano_J[i]);
-    i++;
-    console.log(contador_J);
+
+//Inicio del juego // El Dealer se saca sus cartas hasta llegar a sumar 17 o más
+mano_D[i]=baraja[numeroAleatorio(0,3)][numeroAleatorio(0,12)];//Primera carta
+console.log("PRUEBA de mano_D: " + mano_D);
+contador_D= contador_D + valorarCarta(mano_D[i]);
+puntuacion_D[0] = contador_D; 
+while(contador_D<17){  
+     
+    pedirCarta(mano_D);
+    i = i+1; 
+    contador_D= contador_D + valorarCarta(mano_D[i]); 
+    puntuacion_D[i] = contador_D;           
 }
+for(let k=0;k<mano_D.length;k++){
+    console.log(mano_D[k]);
+    nuevaImagen[k] = document.createElement("img");
+    nuevaImagen[k].setAttribute("src", `./img/${mano_D[k]}.png`);
+    nuevaImagen[k].className = "carta";
+}
+
+console.log(`Esta es la mano del DEALER ${mano_D}`);
+console.log(`El Dealer ha sacado un total de : ${contador_D} puntos`);
+
+pintarTablero();
+// jugadorNombre.textContent = `Puntuación ${name}: `;
+
+pedirButton.addEventListener("click", function () {
+    if(dealer.children.length==mano_D.length){
+        pedirCarta(mano_J);
+        console.log(mano_J);
+        nuevaImagenJugador[l] = document.createElement("img");
+        nuevaImagenJugador[l].setAttribute("src", `./img/${mano_J[l]}.png`);
+        nuevaImagenJugador[l].className = "carta";
+        jugador.appendChild(nuevaImagenJugador[l]);    
+        contador_J=contador_J+ valorarCarta(mano_J[l]);
+        puntuacion_J[l]=contador_J;
+        puntuacionJugador.textContent =`Puntuacion Jugador: ${puntuacion_J[l]}`;    
+        l=l+1;
+        if((contador_J>contador_D)&&contador_J<22){
+            let resultado = document.createElement("div");
+            tablero.appendChild(resultado);
+            resultado.textContent = `${name} ha ganado`;
+            resultado.className = "resultado";
+        }
+        if(contador_J>21){
+            let resultado = document.createElement("div");
+            tablero.appendChild(resultado);
+            resultado.textContent = `${name} ha perdido`;
+            resultado.className = "resultado";
+        }        
+    }      
+});
+
+
+
+
